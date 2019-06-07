d4c178ac9d16f38b98af2d8b5dbf83716b71de24
Kingaa6 <Kingaa6@gmail.com>
2019-06-07 07:55:29 -0700

    Project init

---
+++ hash-tables/ex1/ex1.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "hashtable.h"
+#include "ex1.h"
+
+Answer *get_indices_of_item_weights(int *weights, int length, int limit)
+{
+  HashTable *ht = create_hash_table(16);
+
+  /* YOUR CODE HERE */
+
+  return NULL;
+}
+
+void print_answer(Answer *answer)
+{
+  if (answer != NULL) {
+    printf("%d %d\n", answer->index_1, answer->index_2);
+  } else {
+    printf("NULL\n");
+  }
+}
+
+#ifndef TESTING
+int main(void)
+{
+  // TEST 1
+  int weights_1 = {9};
+  Answer *answer_1 = get_indices_of_item_weights(&weights_1, 1, 9);
+  print_answer(answer_1);  // NULL
+
+  // TEST 2
+  int weights_2[] = {4, 4};
+  Answer* answer_2 = get_indices_of_item_weights(weights_2, 2, 8);
+  print_answer(answer_2);  // {1, 0}
+
+  // TEST 3
+  int weights_3[] = {4, 6, 10, 15, 16};
+  Answer* answer_3 = get_indices_of_item_weights(weights_3, 5, 21);
+  print_answer(answer_3);  // {3, 1}
+
+  // TEST 4
+  int weights_4[] = {12, 6, 7, 14, 19, 3, 0, 25, 40};
+  Answer* answer_4 = get_indices_of_item_weights(weights_4, 9, 7);
+  print_answer(answer_4);  // {6, 2}
+
+  return 0;
+}
+
+#endif
---
+++ hash-tables/ex2/ex2.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "hashtable.h"
+#include "ex2.h"
+
+char **reconstruct_trip(Ticket **tickets, int length)
+{
+  HashTable *ht = create_hash_table(length);
+  char **route = malloc(length * sizeof(char *));
+
+  /* YOUR CODE HERE */
+
+  return route;
+}
+
+void print_route(char **route, int length)
+{
+  for (int i = 0; i < length; i++) {
+    printf("%s\n", route[i]);
+  }
+}
+
+
+
+#ifndef TESTING
+int main(void)
+{
+  // Short test
+  Ticket **tickets = malloc(3 * sizeof(Ticket *));
+
+  Ticket *ticket_1 = malloc(sizeof(Ticket));
+  ticket_1->source = "NONE";
+  ticket_1->destination = "PDX";
+  tickets[0] = ticket_1;
+
+  Ticket *ticket_2 = malloc(sizeof(Ticket));
+  ticket_2->source = "PDX";
+  ticket_2->destination = "DCA";
+  tickets[1] = ticket_2;
+
+  Ticket *ticket_3 = malloc(sizeof(Ticket));
+  ticket_3->source = "DCA";
+  ticket_3->destination = "NONE";
+  tickets[2] = ticket_3;
+
+  print_route(reconstruct_trip(tickets, 3), 3); // PDX, DCA, NONE
+
+  for (int i = 0; i < 3; i++) {
+    free(tickets[i]);
+  }
+
+  free(tickets);
+
+  return 0;
+}
+#endif
